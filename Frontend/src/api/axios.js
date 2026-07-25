4b315a8c08f4bab0c0aa9f4b7f7acaddf39f7180
Zaid Nagori <nagorizaid786@gmail.com>
2026-07-25 12:22:55 +0530

    Fix production API URL

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,24 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "https://lead-management-system-2-lqqd.onrender.com",
+// });
+
+// API.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+
+//   return config;
+// });
+
+// export default API;
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://lead-management-system-2-lqqd.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
